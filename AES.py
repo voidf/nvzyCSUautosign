2159b9efcf14028f1b76c8ea5e74af59876543bb
Kaguya <kaguya@Kaguya.cn>
2021-08-27 16:08:14 +0800

    add: 祖传代码和async改进

---
+++ AES.py
@@ -0,0 +1,173 @@
+from loguru import logger
+try:
+    from Cryptodome.Cipher import AES
+except:
+    from Crypto.Cipher import AES
+
+from binascii import b2a_hex, a2b_hex
+import base64
+import random
+from bs4 import BeautifulSoup
+import aiohttp
+import requests
+from requests import session
+
+# ses = session()
+
+saltStorage = None
+exeStorage = None
+
+
+def randomaesstring(x: int):
+    return ''.join(random.choices("ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678", k=x))
+
+
+class prpcrypt():
+
+    @staticmethod
+    def pad(s):
+        return s + (AES.block_size - len(s) % AES.block_size) * chr(AES.block_size - len(s) % AES.block_size)
+    # 定义 padding 即 填充 为PKCS7
+
+    @staticmethod
+    def unpad(s):
+        return s[0:-ord(s[-1])]
+
+    def __init__(self, key):
+        self.key = key
+        self.mode = AES.MODE_CBC
+        # AES的加密模式为CBC
+
+    def encrypt(self, text, iv):
+        text = prpcrypt.pad(text)
+        cryptor = AES.new(self.key.encode('utf-8'),
+                          self.mode, iv.encode('utf-8'))
+        # 第二个self.key 为 IV 即偏移量
+        x = len(text) % 8
+        if x != 0:
+            text = text + '\0' * (8 - x)  # 不满16，32，64位补0
+        # print(text)
+        self.ciphertext = cryptor.encrypt(text.encode('utf-8'))
+        return base64.standard_b64encode(self.ciphertext).decode("utf-8")
+
+    def decrypt(self, text):
+        cryptor = AES.new(self.key, self.mode, self.key)
+        de_text = base64.standard_b64decode(text)
+        plain_text = cryptor.decrypt(de_text)
+        st = str(plain_text.decode("utf-8")).rstrip('\0')
+        out = prpcrypt.unpad(st)
+        return out
+
+@logger.catch
+def generate_auth(pw: str, key: str):
+    iv = randomaesstring(16)
+    word = randomaesstring(64) + pw
+    pr = prpcrypt(key)
+    return pr.encrypt(word, iv)
+
+@logger.catch
+def get_aes_salt(ses: session):
+    # headers = {
+    #     "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    #     "Accept-Encoding":"gzip, deflate, br",
+    #     "Accept-Language":"zh-CN,zh;q=0.9",
+    #     "Cache-Control":"no-cache",
+    #     "Connection":"keep-alive",
+    #     "DNT":"1",
+    #     "Host":"ca.csu.edu.cn",
+    #     "Pragma":"no-cache",
+    #     "Sec-Fetch-Dest":"document",
+    #     "Sec-Fetch-Mode":"navigate",
+    #     "Sec-Fetch-Site":"none",
+    #     "Sec-Fetch-User":"?1",
+    #     "Upgrade-Insecure-Requests":"1",
+    #     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
+    # }
+    req = ses.get('https://ca.csu.edu.cn/authserver/login?service=https%3A%2F%2Fwxxy.csu.edu.cn%2Fa_csu%2Fapi%2Fcas%2Findex%3Fredirect%3Dhttps%253A%252F%252Fwxxy.csu.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex%253Ffrom%253Dhistory%26from%3Dwap')
+
+    B = BeautifulSoup(req.text, "html.parser")
+    saltobj = B.find('input', attrs={'id': 'pwdEncryptSalt'})
+    exeobj = B.find('input', attrs={'id': 'execution'})
+
+    global saltStorage, exeStorage
+    if saltobj:
+        saltStorage = saltobj
+    else:
+        saltobj = saltStorage
+    if exeobj:
+        exeStorage = exeobj
+    else:
+        exeobj = exeStorage
+    # print(saltobj['value'])
+    # print(exeobj['value'])
+    return saltobj['value'], exeobj['value']
+
+@logger.catch
+async def aget_aes_salt(ses: aiohttp.ClientSession):
+    async with ses.get('https://ca.csu.edu.cn/authserver/login?service=https%3A%2F%2Fwxxy.csu.edu.cn%2Fa_csu%2Fapi%2Fcas%2Findex%3Fredirect%3Dhttps%253A%252F%252Fwxxy.csu.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex%253Ffrom%253Dhistory%26from%3Dwap') as resp:
+        req = await resp.text()
+    # req = ses.get()
+    B = BeautifulSoup(req, "html.parser")
+    saltobj = B.find('input', attrs={'id': 'pwdEncryptSalt'})
+    exeobj = B.find('input', attrs={'id': 'execution'})
+
+    global saltStorage, exeStorage
+    if saltobj:
+        saltStorage = saltobj
+    else:
+        saltobj = saltStorage
+    if exeobj:
+        exeStorage = exeobj
+    else:
+        exeobj = exeStorage
+    return saltobj['value'], exeobj['value']
+
+@logger.catch
+def r2(ses: session, user: str, pw: str):
+    salt, exe = get_aes_salt(ses)
+    form = {
+        "username": user,
+        "password": generate_auth(pw, salt),
+        "captcha": None,
+        # "rememberMe":True,
+        "_eventId": "submit",
+        "cllt": "userNameLogin",
+        "dllt": "generalLogin",
+        "lt": None,
+        "execution": exe
+    }
+    lnk = 'https://ca.csu.edu.cn/authserver/login?service=https%3A%2F%2Fwxxy.csu.edu.cn%2Fa_csu%2Fapi%2Fcas%2Findex%3Fredirect%3Dhttps%253A%252F%252Fwxxy.csu.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex%253Ffrom%253Dhistory%26from%3Dwap'
+    # https://ca.csu.edu.cn/authserver/login?service=https%3A%2F%2Fwxxy.csu.edu.cn%2Fa_csu%2Fapi%2Fcas%2Findex%3Fredirect%3Dhttps%253A%252F%252Fwxxy.csu.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex%253Ffrom%253Dhistory%26from%3Dwap
+    req = ses.post(lnk, data=form)
+    print(req.history)
+    print(req)
+    # logger.warning(req.text)
+
+@logger.catch
+async def ar2(ses: aiohttp.ClientSession, user: str, pw: str):
+    salt, exe = await aget_aes_salt(ses)
+    form = {
+        "username": user,
+        "password": generate_auth(pw, salt),
+        "captcha": None,
+        "rememberMe": True,
+        "_eventId": "submit",
+        "cllt": "userNameLogin",
+        "dllt": "generalLogin",
+        "lt": None,
+        "execution": exe
+    }
+    lnk = 'https://ca.csu.edu.cn/authserver/login?service=https%3A%2F%2Fwxxy.csu.edu.cn%2Fa_csu%2Fapi%2Fcas%2Findex%3Fredirect%3Dhttps%253A%252F%252Fwxxy.csu.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex%253Ffrom%253Dhistory%26from%3Dwap'
+    async with ses.post(
+        lnk,
+        data=form
+    ) as resp:
+        req = await resp.text()
+        print(req)
+
+    # req = ses.post(lnk, data=form)
+    # print(req.history)
+    # print(req)
+    # print(req.text)
+# if __name__ == "__main__":
+#     r2()
---
+++ CSU2020AutoSign_copied.py
@@ -0,0 +1,194 @@
+# coding=utf-8
+import time
+import datetime
+import json
+import requests
+import re
+import ssl
+from http_config import auth
+import aiohttp
+import urllib.parse
+from bs4 import BeautifulSoup as BS
+import urllib.request
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
+import traceback
+import requests.packages.urllib3.util.ssl_
+from AES import r2, ar2
+from loguru import logger
+requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL'
+
+class MyAdapter(HTTPAdapter):
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = PoolManager(num_pools=connections,
+                                       maxsize=maxsize,
+                                       block=block,
+                                       ssl_version=ssl.PROTOCOL_TLSv1)
+
+ses = requests.session()
+# ses.mount('https://', MyAdapter(max_retries=20))
+
+
+
+ses.headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
+}
+
+@logger.catch
+def get_old_info():
+    auth_session(ses)
+    lnk = 'https://wxxy.csu.edu.cn/ncov/wap/default/index?from=history'
+    hds = {
+        "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Encoding":"gzip, deflate, br",
+        "Accept-Language":"zh-CN,zh;q=0.9",
+        "Cache-Control":"no-cache",
+        "Connection":"keep-alive",
+        "DNT":"1",
+        "Host":"wxxy.csu.edu.cn",
+        "Pragma":"no-cache",
+        "Referer":"https://wxxy.csu.edu.cn/ncov/wap/default/recently",
+        "Upgrade-Insecure-Requests":"1",
+        "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
+    }
+    sr = ses.get(lnk)
+    # logger.critical(sr.text)
+    # print(sr.text)
+    rer = re.findall('''var def = ({.*})''',sr.text)
+    # logger.warning(rer)
+    # print(rer)
+    je = json.loads(rer[0])
+    return je
+
+@logger.catch
+async def agetold(ses: aiohttp.ClientSession):
+    await aauth_session(ses)
+    lnk = 'https://wxxy.csu.edu.cn/ncov/wap/default/index?from=history'
+    async with ses.get(lnk) as resp:
+        sr = await resp.text()
+    rer = re.findall('''var def = ({.*})''',sr)
+    # logger.warning(rer)
+    # print(rer)
+    je = json.loads(rer[0])
+    return je
+
+@logger.catch
+def sleep_until_sign(signtime):
+    slpt = (datetime.datetime.now().timestamp() + 3600 * 8 ) % 86400
+    slpt = 86400 - slpt + signtime
+    print(f'阻塞{slpt}秒')
+    time.sleep(max(1,slpt))
+
+@logger.catch
+async def asleep_until_sign(signtime):
+    slpt = (datetime.datetime.now().timestamp() + 3600 * 8 ) % 86400
+    slpt = 86400 - slpt + signtime
+    print(f'等待{slpt}秒')
+    await asyncio.sleep(max(1,slpt))
+
+@logger.catch
+async def main():
+    signtime = 500 # 在每天的第几秒签到
+    # sleep_until_sign(signtime)
+    async with aiohttp.ClientSession() as ses:
+        while True:
+            try:
+                # sign_res = sign_in(get_old_info())
+                sign_res = await asign_in(ses, await agetold(ses))
+                if sign_res[0] == "{":
+                    print(sign_res)
+                    sign_res = json.loads(sign_res)
+                    if sign_res["e"] == 0:
+                        print("今日签到提交成功，msg:", sign_res)
+                        print(f'{datetime.datetime.now()}')
+                        # sleep_until_sign(signtime)
+                        await asleep_until_sign(signtime)
+                    elif sign_res["e"] == 1 and sign_res["m"] == "今天已经填报了":
+                        print("今天已完成签到，msg:", sign_res)
+                        print(f'{datetime.datetime.now()}')
+                        # sleep_until_sign(signtime)
+                        await asleep_until_sign(signtime)
+                    else:
+                        print("未知的错误，msg:", sign_res)
+                else:
+                    # auth_session()
+                    await aauth_session(ses)
+                # time.sleep(5)
+                await asyncio.sleep(30)
+            except:
+                traceback.print_exc()
+                # time.sleep(5)
+                await asyncio.sleep(30)
+            
+            print("程序退出")
+    
+
+@logger.catch
+def sign_in(je):
+    auth_session()
+    url = "https://wxxy.csu.edu.cn/ncov/wap/default/save"
+    try:
+        
+        je["id"] = int(je["id"])
+        je["date"] = str_time("%Y%m%d")
+        je["created"] = unix_time()
+        jgeo = json.loads(je["geo_api_info"])
+        je["address"] = jgeo['formattedAddress']
+        je["city"] = jgeo['addressComponent']['city']
+        je["province"] = jgeo['addressComponent']['province']
+        je["area"] = ' '.join([
+            jgeo['addressComponent']['province'],
+            jgeo['addressComponent']['city'],
+            jgeo['addressComponent']['district'],
+        ])
+
+        http_result = ses.post(
+            url, data=je, timeout=(2, 30))
+        return http_result.text
+    except requests.exceptions.ReadTimeout:
+        print("requests.exceptions.ReadTimeout:[%s]" % url)
+        return ""
+    except requests.exceptions.ConnectionError:
+        print("requests.exceptions.ConnectionError:[%s]" % url)
+        return ""
+@logger.catch
+async def asign_in(ses: aiohttp.ClientSession, je):
+    await aauth_session(ses)
+    url = "https://wxxy.csu.edu.cn/ncov/wap/default/save"
+    je["id"] = int(je["id"])
+    je["date"] = str_time("%Y%m%d")
+    je["created"] = unix_time()
+    jgeo = json.loads(je["geo_api_info"])
+    je["address"] = jgeo['formattedAddress']
+    je["city"] = jgeo['addressComponent']['city']
+    je["province"] = jgeo['addressComponent']['province']
+    je["area"] = ' '.join([
+        jgeo['addressComponent']['province'],
+        jgeo['addressComponent']['city'],
+        jgeo['addressComponent']['district'],
+    ])
+    async with ses.post(
+        url, data=je, timeout=5
+    ) as resp:
+        return await resp.text()
+
+def unix_time():
+    return int(datetime.datetime.now().timestamp())
+
+@logger.catch
+def auth_session():
+    r2(ses, auth['userName'], auth['passWord'])
+    
+@logger.catch
+async def aauth_session(ses: aiohttp.ClientSession):
+    await ar2(ses, auth['userName'], auth['passWord'])
+
+
+def str_time(pattern='%Y-%m-%d %H:%M:%S'):
+    return datetime.datetime.now().strftime(pattern)
+
+import asyncio
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
