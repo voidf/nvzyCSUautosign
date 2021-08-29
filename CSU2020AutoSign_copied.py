bdc4096120c29b494819faad95d4fdb1d0748f36
Kaguya <kaguya@Kaguya.cn>
2021-08-29 12:49:10 +0800

    add: 加个不知道有没有用的UA

--- CSU2020AutoSign_copied.py
+++ CSU2020AutoSign_copied.py
@@ -55,7 +55,11 @@ class AutoSign:
 
 
     def __init__(self, usr: str, pwd: str):
-        self.ses = aiohttp.ClientSession()
+        self.ses = aiohttp.ClientSession(
+            headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'
+            }
+        )
         self.usr = usr
         self.pwd = pwd
         self.alive = True
